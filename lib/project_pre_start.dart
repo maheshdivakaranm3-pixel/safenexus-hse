7b0753f7f5b586bdbfe6dc700cb7a282cf491e30
maheshdivakaranm3-pixel <maheshdivakaran.m3@gmail.com>
2026-09-10 21:54:42 +0530

    Refactor checklist count getters and clean up code

--- lib/project_pre_start.dart
+++ lib/project_pre_start.dart
@@ -493,16 +493,10 @@ class _ProjectPreStartPageState extends State<ProjectPreStartPage> {
     final completed = checks.where((value) => value).length;
     return ((completed / checks.length) * 100).round();
   }
- }
 
-int get _checklistCompletedCount =>
-    _checklistStatus.values.where((value) => value == 'Completed').length;
+  int get _checklistCompletedCount =>
+      _checklistStatus.values.where((value) => value == 'Completed').length;
 
-@override
-Widget build(BuildContext context) {
-
-  int get _checklistApplicableCount =>
-      _checklistStatus.values.where((value) => value != 'N/A').length;
 
   @override
   Widget build(BuildContext context) {
