3a5995558a9525b4ff9b4b69c144f52848896fbc
maheshdivakaranm3-pixel <maheshdivakaran.m3@gmail.com>
2026-09-14 10:39:53 +0530

    Add files via upload

---
+++ lib/dubai_hse_topic_router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'pages/dubai/scaffolding_safety_page.dart';
+import 'pages/dubai/excavation_trenching_page.dart';
+import 'pages/dubai/lifting_operations_page.dart';
+
+class DubaiDedicatedPageRouter {
+  static Widget? pageFor(String id) {
+    switch (id) {
+      case 'dubai_scaffolding_safety':
+        return const ScaffoldingSafetyPage();
+      case 'dubai_excavation_trenching':
+        return const ExcavationTrenchingPage();
+      case 'dubai_lifting_operations':
+        return const LiftingOperationsPage();
+      default:
+        return null;
+    }
+  }
+}
