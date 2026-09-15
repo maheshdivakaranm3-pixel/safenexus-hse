d367ef2779684c4b3ae5a07c1820a2760cc13f23
maheshdivakaranm3-pixel <maheshdivakaran.m3@gmail.com>
2026-09-15 14:27:46 +0530

    Delete lib/pages/dubai/dubai_hse_part1_router.dart

--- lib/pages/dubai/dubai_hse_part1_router.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'dubai_hse_part1_page.dart';
-
-class DubaiHsePart1Router {
-  static Widget pageFor(String id) => DubaiHsePart1TopicPage(topicId: id);
-}
