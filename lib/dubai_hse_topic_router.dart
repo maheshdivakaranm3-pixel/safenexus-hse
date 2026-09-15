bcb54ab799f7cccb72118e149c6051ca24f3166a
maheshdivakaranm3-pixel <maheshdivakaran.m3@gmail.com>
2026-09-15 21:21:20 +0530

    Add files via upload

--- lib/dubai_hse_topic_router.dart
+++ lib/dubai_hse_topic_router.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'dubai_hse_detail_page.dart';
 import 'models/reference_topic.dart';
-
 import 'pages/dubai/dubai_construction_safety_framework_page.dart';
+import 'pages/dubai/dubai_hse_management_page.dart';
 import 'pages/dubai/scaffolding_safety_page.dart';
 import 'pages/dubai/excavation_trenching_page.dart';
 import 'pages/dubai/lifting_operations_page.dart';
@@ -13,21 +13,14 @@ class DubaiHsePartRouter {
     switch (topic.id) {
       case 'dubai_construction_safety':
         return const DubaiConstructionSafetyFrameworkPage();
-
-      // HSE Management System opens its normal detail page first.
-      // The page contains an explicit Advanced Learning button.
       case 'dubai_hse_management':
-        return DubaiHseDetailPage(topic: topic);
-
+        return const DubaiHseManagementPage();
       case 'dubai_scaffolding':
         return const ScaffoldingSafetyPage();
-
       case 'dubai_excavation':
         return const ExcavationTrenchingPage();
-
       case 'dubai_lifting':
         return const LiftingOperationsPage();
-
       default:
         return DubaiHseDetailPage(topic: topic);
     }
