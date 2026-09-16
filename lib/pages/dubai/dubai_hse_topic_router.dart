9bd2e595cf64f9433472663d3d588002de62ab0c
maheshdivakaranm3-pixel <maheshdivakaran.m3@gmail.com>
2026-09-16 08:41:31 +0530

    Update dubai_hse_topic_router.dart

--- lib/pages/dubai/dubai_hse_topic_router.dart
+++ lib/pages/dubai/dubai_hse_topic_router.dart
@@ -2,32 +2,45 @@ import 'package:flutter/material.dart';
 
 import '../../dubai_hse_detail_page.dart';
 import '../../models/reference_topic.dart';
+
 import 'dubai_construction_safety_framework_page.dart';
 import 'dubai_hse_construction_hse_plan_page.dart';
 import 'dubai_hse_management_page.dart';
+import 'scaffolding_safety_page.dart';
 import 'excavation_trenching_page.dart';
 import 'lifting_operations_page.dart';
-import 'scaffolding_safety_page.dart';
 
-/// Compatibility router for the accidental legacy path.
-/// The canonical router remains: lib/dubai_hse_topic_router.dart
+/// Compatibility router for the legacy path.
+///
+/// Canonical router:
+/// lib/dubai_hse_topic_router.dart
 class DubaiHsePartRouter {
   static Widget pageFor(ReferenceTopic topic) {
     switch (topic.id) {
       case 'dubai_construction_safety':
         return const DubaiConstructionSafetyFrameworkPage();
+
       case 'dubai_hse_management':
         return const DubaiHseManagementPage();
+
       case 'dubai_hse_plan':
-        return DubaiHseConstructionHsePlanPage(topic: topic);
+        return DubaiHseConstructionHsePlanPage(
+          topic: topic,
+        );
+
       case 'dubai_scaffolding':
         return const ScaffoldingSafetyPage();
+
       case 'dubai_excavation':
         return const ExcavationTrenchingPage();
+
       case 'dubai_lifting':
         return const LiftingOperationsPage();
+
       default:
-        return DubaiHseDetailPage(topic: topic);
+        return DubaiHseDetailPage(
+          topic: topic,
+        );
     }
   }
 }
