533c556780f38f72cd6ea2217ef1305d85f866f0
maheshdivakaranm3-pixel <maheshdivakaran.m3@gmail.com>
2026-09-21 22:22:44 +0530

    Add files via upload

--- lib/abu_dhabi_gold_topic_router.dart
+++ lib/abu_dhabi_gold_topic_router.dart
@@ -6,17 +6,10 @@ import 'data/abu_dhabi_5AB_to_5AE_plant_haulage_compaction_gold.dart';
 import 'data/abu_dhabi_5AF_to_5AI_paver_trencher_compressor_generator_gold.dart';
 import 'data/abu_dhabi_5S_to_5V_mobile_material_handling_gold.dart';
 import 'data/abu_dhabi_5W_to_5Z_earthmoving_gold.dart';
-import 'data/abu_dhabi_confined_spaces_gold.dart';
-import 'data/abu_dhabi_formwork_gold.dart';
+import 'data/abu_dhabi_specialist_hse_complete_gold.dart';
 import 'data/abu_dhabi_forklift_powered_lift_trucks_gold.dart';
 import 'data/abu_dhabi_mewp_gold.dart';
-import 'data/abu_dhabi_permit_to_work_gold.dart';
-import 'data/abu_dhabi_power_tools_gold.dart';
-import 'data/abu_dhabi_safety_in_heat_gold.dart';
-import 'data/abu_dhabi_scaffolding_gold.dart';
-import 'data/abu_dhabi_working_at_height_gold.dart';
 import 'data/abu_dhabi_crane_lifting_book_gold.dart';
-import 'data/abu_dhabi_steps_5N_to_5Q_book_gold.dart';
 import 'models/reference_topic.dart';
 
 /// Step 5AK-A: central Gold Standard router.
